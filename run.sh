cffa775d44858db69fec99d066955f4144e05ead
jingslunt <jingslunt@qq.com>
2022-12-20 21:10:14 +0800

    Delete run.sh

--- run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-kafkacat -b 10.238.239.200:9092 -t mp-biz -G logstash -p 0   -C \
-|jq -r -c '{level,scenario,appName}|select(.level=="ERROR")' \
-| grep -oP '^\s*(\{(?:[^{}]|(?1))*\})\s*$' \
-|awk  '{count[$1]++}END {for (minute in count) print "logs_error"minute,count[minute]}' \
-|httpstdin
