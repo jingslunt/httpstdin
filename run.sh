acab18441c804014919bc68b143214e02a030844
jingslunt <jingslunt@qq.com>
2022-12-20 19:06:18 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@ kafkacat -b 10.238.239.200:9092 -t mp-biz -G logstash -p 0   -C \
 |jq -r -c '{level,scenario,appName}|select(.level=="ERROR")' \
 | grep -oP '^\s*(\{(?:[^{}]|(?1))*\})\s*$' \
 |awk  '{count[$1]++}END {for (minute in count) print "logs_error"minute,count[minute]}' \
-|httpstdin
+|./httpstdin
